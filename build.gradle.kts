90f0385e4c8525017564ed924c9b9b71effe66e8
jplumb <jplumb@newrelic.com>
2020-09-21 10:45:20 -0700

    remove redundanct signing config

--- build.gradle.kts
+++ build.gradle.kts
@@ -99,9 +99,6 @@ configure<PublishingExtension> {
                 val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
                 val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
                 url = if (isRelease) releasesRepoUrl else snapshotsRepoUrl
-                configure<SigningExtension> {
-                    sign(publications["mavenJava"])
-                }
             }
             credentials {
                 username = project.properties["sonatypeUsername"] as String?
